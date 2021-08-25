3c20e1df04eef1774628b00278d8793289b396f2
eldrige <peldrige8@gmail.com>
2021-08-25 05:47:10 +0100

    work on header

---
+++ src/components/TopProducts.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TopProducts = () => {
+  return <div></div>;
+};
+
+export default TopProducts;
--- src/screens/Hero.js
+++ src/screens/Hero.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 
 const Hero = () => {
   return (
-    <section className="container-fluid" id="hero">
-      <div className="navbar"></div>
+    <section id="hero">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="img-container">
+            {/* <Image src="/images/eggs.jpg" fluid style={{ height: '50%' }} /> */}
+          </div>
+        </div>
+        <div className="col-md-6 my-5">
+          <div className="hero-content">
+            <h1>
+              Pasta <br />
+              is our <br />
+              happy food
+            </h1>
+            <h3>No machine required.</h3>
+            <button>Go to Shop</button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
